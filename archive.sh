45b20f4ac1082a28e5494d8625745ec1ba784c3e
Benjamin Heng <benjheng@gmail.com>
2022-08-18 22:46:38 +0800

    Update gamemode_40l.csv

---
+++ archive.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git pull
+sqlite3 $1 "select played_at, time_ms, finesse_percent, total_pieces, pieces_per_second from gamemode_40l order by played_at desc" --csv > gamemode_40l.csv
+git add gamemode_40l.csv
